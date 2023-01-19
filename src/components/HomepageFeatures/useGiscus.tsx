9d40ecc601455aed3a921f9fe02a828b85ec88ca
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-01-19 18:56:13 +0800

    add giscus

---
+++ src/components/HomepageFeatures/useGiscus.tsx
@@ -0,0 +1,22 @@
+import Giscus from '@giscus/react';
+import React from 'react';
+
+export default function UseGiscus() {
+    return (
+        <Giscus
+            id="comments"
+            repo="GavinLin512/Grayson-docs"
+            repoId="R_kgDOIxMi7g"
+            category="Announcements"
+            categoryId="DIC_kwDOIxMi7s4CTrUP"
+            mapping="pathname"
+            term="Welcome to @giscus/react component!"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="bottom"
+            theme="preferred_color_scheme"
+            lang="zh-TW"
+            loading="lazy"
+        />
+    );
+}
