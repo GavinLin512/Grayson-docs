b9416c2932949bdcbe5f6d60e3ccfb1967d41741
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2024-12-30 16:17:17 +0800

    adjust Argos CI spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,12 +3,12 @@ import { argosScreenshot } from "@argos-ci/playwright";
 
 const pages = [
     { name: "homepage", path: "/" },
-    { name: "pricing", path: "/docs/vue/lifecycle_hooks" },
+    { name: "vue_lifecycle_hooks", path: "/docs/vue/lifecycle_hooks" },
 ];
 
 for (const { name, path } of pages) {
     test(`Run Argos screenshot on ${name} (${path})`, async ({ page }) => {
         await page.goto(`http://localhost:3000${path}`);
-        await argosScreenshot(page, path);
+        await argosScreenshot(page, name);
     });
 }
