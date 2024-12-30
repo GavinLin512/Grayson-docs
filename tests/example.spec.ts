ade797c9805b8fef9166838668323b76afac14ca
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2024-12-30 16:28:46 +0800

    adjust Argos CI spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,9 +6,15 @@ const pages = [
     { name: "vue_lifecycle_hooks", path: "/docs/vue/lifecycle_hooks" },
 ];
 
+const viewports = [
+    "macbook-15", // Use device preset
+    // { width: 800, height: 600 }, // Specify dimensions directly
+    // { preset: "ipad-2", orientation: "landscape" }, // Device preset with orientation
+],
+
 for (const { name, path } of pages) {
-    test(`Run Argos screenshot on ${name} (${path})`, async ({ page }) => {
+    test(`Run Argos screenshot on ${name} (${path})`, async ({ page}) => {
         await page.goto(`http://localhost:3000${path}`);
-        await argosScreenshot(page, name);
+        await argosScreenshot(page, name, viewports: viewports);
     });
 }
