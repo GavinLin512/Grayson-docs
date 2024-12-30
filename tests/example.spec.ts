03581079e5ff4a11540774c5a7a89a79e0d3c3ce
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2024-12-30 16:43:26 +0800

    adjust Argos CI spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -10,7 +10,7 @@ const viewports = [
     "macbook-15", // Use device preset
     // { width: 800, height: 600 }, // Specify dimensions directly
     // { preset: "ipad-2", orientation: "landscape" }, // Device preset with orientation
-],
+];
 
 for (const { name, path } of pages) {
     test(`Run Argos screenshot on ${name} (${path})`, async ({ page}) => {
